01600ca8413cc28adfe12872c77abb48e760c67f
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-17 12:14:27 -0700

    First commit.

---
+++ sead40_hw3Tests/sead40_hw3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  sead40_hw3Tests.swift
+//  sead40_hw3Tests
+//
+//  Created by Joao Paulo Galvao Alves on 8/17/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class sead40_hw3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
