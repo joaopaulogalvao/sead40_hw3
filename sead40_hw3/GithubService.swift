533c9cca441e0e83874f8870dcbfa6d5f52b1462
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-18 09:46:30 -0700

    Add MenuTableViewController, GitHubService and its code and RepoSearchViewController.

---
+++ sead40_hw3/GithubService.swift
@@ -0,0 +1,28 @@
+//
+//  GithubService.swift
+//  sead40_hw3
+//
+//  Created by Joao Paulo Galvao Alves on 8/18/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
+
+class GithubService {
+  class func repositoriesForSearchTerm(searchTerm : String) {
+    let baseURL = "http://localhost:3000"
+    let finalURL = baseURL + "?q=\(searchTerm)"
+    if let url = NSURL(string: finalURL) {
+      NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+        if let error = error {
+          println("error")
+        } else if let httpResponse = response as? NSHTTPURLResponse {
+          println(httpResponse)
+          
+          
+        }
+      }).resume()
+    }
+    
+  }
+}
