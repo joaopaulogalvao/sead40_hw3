523cb375bf7684bc5273b65706630dcdfa8a91eb
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-17 18:22:37 -0700

    Add UITableViewController and search.Add user and repos model.

---
+++ sead40_hw3/Repos.swift
@@ -0,0 +1,13 @@
+//
+//  Repos.swift
+//  sead40_hw3
+//
+//  Created by Joao Paulo Galvao Alves on 8/17/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
+
+class Repos {
+  
+}
---
+++ sead40_hw3/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  sead40_hw3
+//
+//  Created by Joao Paulo Galvao Alves on 8/17/15.
+//  Copyright (c) 2015 jalvestech. All rights reserved.
+//
+
+import Foundation
+
+class User {
+  
+
+  
+}
