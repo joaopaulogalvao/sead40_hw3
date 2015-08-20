50927c09925fe5923238d12ffc5b8863f0e965d5
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-19 19:36:57 -0700

    Connect delegate and datasource. Add UICollectionViewDataSource and delegate methods.

--- sead40_hw3/UserSearchViewController.swift
+++ sead40_hw3/UserSearchViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class UserSearchViewController: UIViewController {
+class UserSearchViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
 
+  var userResults = [User]()
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var searchBarUser: UISearchBar!
+  
+  
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+     self.collectionView.dataSource = self
+      self.collectionView.delegate = self
+      self.searchBarUser.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,3 +44,58 @@ class UserSearchViewController: UIViewController {
     */
 
 }
+
+extension UserSearchViewController : UISearchBarDelegate {
+  
+  func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+    GithubService.usersForSearchTerm(self.searchBarUser.text) { (errorDescription, users) -> (Void) in
+      
+      if let users = users {
+        NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+          println(users)
+          self.userResults = users
+          self.collectionView.reloadData()
+        })
+      }
+    }
+  }
+  
+}
+
+extension UserSearchViewController : UICollectionViewDelegate {
+  
+  
+  
+  
+}
+
+extension UserSearchViewController : UICollectionViewDataSource{
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return userResults.count
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("userCell", forIndexPath: indexPath) as! CollectionViewUserCell
+    var userImage = userResults[indexPath.row]
+    println("Users image:\(userImage)")
+    
+    return cell
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
